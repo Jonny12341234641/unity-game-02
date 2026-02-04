70daac231d7badb8bdff248ef8cc89bb8852723c
Kavindu <kavinduuni@gmail.com>
2026-02-04 14:33:38 +0530

    feat: Add PlayerController script for player movement and boosting, and create the initial Level 1 scene.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,37 @@
+using UnityEditor.Toolbars;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 15f;
+    [SerializeField] float boostSpeed = 20f;
     InputAction moveAction;
     Rigidbody2D myRigidbody2D;
+    SurfaceEffector2D surfaceEffector2D;
+
+    Vector2 moveVector;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveVector;
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void RotatePlayer()
+    {
+        // Vector2 moveVector;
         moveVector = moveAction.ReadValue<Vector2>();
         if (moveVector.x < 0)
         {
@@ -28,5 +42,17 @@ public class PlayerController : MonoBehaviour
             myRigidbody2D.AddTorque(-torqueAmount);
         }
 
+    }
+
+    void BoostPlayer()
+    {
+        if (moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
     }
 }
